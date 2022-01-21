30645973190ae45ffd312e171a23314d5130ef30
Thaylor Carvalho <thaylorcs@outlook.com>
2022-01-21 00:14:12 -0300

    DELETE OK

--- Classes/Service/UsuariosService.php
+++ Classes/Service/UsuariosService.php
@@ -2,7 +2,7 @@
 
 namespace Service;
 
-use http\Exception\InvalidArgumentException;
+use InvalidArgumentException;
 use Repository\UsuariosRepository;
 use Util\ConstantesGenericasUtil;
 
@@ -10,6 +10,7 @@ class UsuariosService
 {
     public const TABELA = 'usuarios';
     public const RECURSOS_GET = ['listar'];
+    public const RECURSOS_DELETE = ['deletar'];
 
     private array $dados = [];
 
@@ -29,7 +30,29 @@ class UsuariosService
         if (in_array($recurso, self::RECURSOS_GET)) {
             $retorno = $this->dados['id'] > 0 ? $this->getOneByKey() : $this->$recurso();
         } else {
-            throw new \InvalidArgumentException(ConstantesGenericasUtil::MSG_ERRO_RECURSO_INEXISTENTE);
+            throw new InvalidArgumentException(ConstantesGenericasUtil::MSG_ERRO_RECURSO_INEXISTENTE);
+        }
+
+        if ($retorno === null) {
+            throw new InvalidArgumentException(ConstantesGenericasUtil::MSG_ERRO_GENERICO);
+        }
+
+        return $retorno;
+
+    }
+
+    public function validarDelete()
+    {
+        $retorno = null;
+        $recurso = $this->dados['recurso'];
+        if (in_array($recurso, self::RECURSOS_DELETE)) {
+            if ($this->dados['id'] > 0) {
+                $retorno = $this->$recurso();
+            } else {
+                throw new InvalidArgumentException(ConstantesGenericasUtil::MSG_ERRO_ID_OBRIGATORIO);
+            }
+        } else {
+            throw new InvalidArgumentException(ConstantesGenericasUtil::MSG_ERRO_RECURSO_INEXISTENTE);
         }
 
         if ($retorno === null) {
@@ -48,5 +71,10 @@ class UsuariosService
     private function listar()
     {
         return $this->usuariosRepository->getMySQL()->getAll(self::TABELA);
+    }
+
+    private function deletar()
+    {
+        return $this->usuariosRepository->getMySQL()->delete(self::TABELA, $this->dados['id']);
     }
 }
